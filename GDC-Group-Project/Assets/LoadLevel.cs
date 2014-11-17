8e0db50bb107b89a35deb40e8b006c3eca749cdb
Trevor Clelland <trevy_17@hotmail.com>
2014-11-17 14:14:01 -0800

    Added basic 2d elements

---
+++ GDC-Group-Project/Assets/LoadLevel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevel : MonoBehaviour {
+
+
+	public string level;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) 
+	{
+		Application.LoadLevel (level);
+	}
+}
