c54a86397d014d7ad0be26c55645b614e32e59a1
Sami Ma <sami.ma3@gmail.com>
2014-12-22 21:48:24 -0800

    Moved the scripts to the scripts folder and might have improved the agent AI, as well as updated the related Prefab. Created a messageOnCollisionExit script with the help of the messageOnCollision script. Created a script to destroy projectiles on contact with Dino.

---
+++ GDC-Group-Project/Assets/Scripts/GDC Member Scripts/Agent/AgentAI.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentAI : MonoBehaviour {
+	Animator animator;
+	Transform agent;
+	bool engage = false;
+	public EnemyGun gun; // The gun child class of the agent.
+	public float speed; // The speed of the agent.
+	public float fireInterval;
+	public Transform player; // The position of the play character.
+
+	public void Start() {
+		animator = GetComponent<Animator> ();
+		agent = GetComponent<Transform> ();
+		move ();
+	}
+
+	public void shoot() {
+		gun.Fire ();
+	}
+
+	void FixedUpdate() {
+		move ();
+	}
+
+	void move() {
+		if (engage == false) {
+						if (player.position.x > agent.position.x) {
+								rigidbody2D.velocity = Vector2.right * speed;
+								
+						} else if (player.position.x < agent.position.x) {
+								rigidbody2D.velocity.Set (-speed, 0);
+						}
+						animator.SetFloat ("speed", 1);
+				} else {
+						animator.SetFloat ("speed", 0);
+				}
+	}
+
+	public void EngagePlayer() {
+		if (engage == false) {
+						InvokeRepeating ("shoot", 0, fireInterval);
+						engage = true;
+				}
+		rigidbody2D.velocity = Vector2.zero;
+	}
+
+	public void DeengagePlayer() {
+		engage = false;
+		CancelInvoke ();
+	}
+}
---
+++ GDC-Group-Project/Assets/Scripts/messageOnCollisionExit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class messageOnCollisionExit : MonoBehaviour {
+
+	public bool onTrigger = false;
+	public string message = "Activate";
+	public GameObject[] targets;
+	public SendMessageOptions options = SendMessageOptions.DontRequireReceiver;
+	
+	void OnCollisionExit2D(Collision2D other)
+	{
+		foreach(GameObject g in targets)
+		{
+			g.SendMessage(message,options);
+		}
+	}
+	
+	void OnCollisionExit(Collision other)
+	{
+		foreach(GameObject g in targets)
+		{
+			g.SendMessage(message,options);
+		}
+	}
+	
+	void OnTriggerExit2D(Collider2D other) {
+		foreach(GameObject g in targets)
+		{
+			g.SendMessage(message,options);
+		}
+	}
+}
